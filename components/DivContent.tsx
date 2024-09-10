fb3e70dc052f67ea5dd65d834d971f6f241452ec
Park JunYong <dyden119@gmail.com>
2024-09-10 16:41:39 +0900

    design: 언더디브 호버이펙트

--- components/DivContent.tsx
+++ components/DivContent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function DivContent({
   children,
@@ -8,14 +8,61 @@ export default function DivContent({
   hoverBg,
   filter = false,
   contain = false,
+  slide1 = false,
+  slide2 = false,
 }: {
   children?: ReactNode;
   bg?: string;
   hoverBg?: string;
   filter?: boolean;
   contain?: boolean;
+  slide1?: boolean;
+  slide2?: boolean;
 }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slides1 = [
+    "slide0.png",
+    "slide1.png",
+    "slide2.png",
+    "slide3.png",
+    "slide4.png",
+    "slide5.png",
+    "slide6.png",
+    "slide7.png",
+  ];
+
+  const slides2 = [
+    "slide2-0.png",
+    "slide2-1.png",
+    "slide2-2.png",
+    "slide2-3.png",
+    "slide2-4.png",
+    "slide2-5.png",
+    "slide2-6.png",
+    "slide2-7.png",
+  ];
+
+  useEffect(() => {
+    let slideInterval = null;
+
+    if ((isHovered && slide1) || (isHovered && slide2)) {
+      slideInterval = setInterval(() => {
+        setCurrentSlide((prev) => (prev + 1) % slides1.length);
+      }, 300);
+    } else {
+      setCurrentSlide(0);
+    }
+
+    return () => {
+      if (slideInterval) {
+        console.log("인터벌 정리 되는지 확인");
+        clearInterval(slideInterval);
+      }
+    };
+    // eslint-disable-next-line
+  }, [isHovered, slide1, slide2]);
 
   return (
     <div
@@ -24,7 +71,13 @@ export default function DivContent({
       }`}
       style={{
         backgroundImage: `url(${
-          isHovered && hoverBg ? `/div/${hoverBg}` : `/div/${bg}`
+          slide1 && isHovered
+            ? `/div/${slides1[currentSlide]}`
+            : slide2 && isHovered
+            ? `/div/${slides2[currentSlide]}`
+            : isHovered && hoverBg
+            ? `/div/${hoverBg}`
+            : `/div/${bg}`
         })`,
         filter: filter ? (isHovered ? "none" : "grayscale(100%)") : "",
       }}
--- components/UnderDiv.tsx
+++ components/UnderDiv.tsx
@@ -4,37 +4,50 @@ export default function UnderDiv() {
   return (
     <div className="aspect-[16/5] grid grid-cols-12 grid-rows-4">
       <div className="col-span-6 row-span-2">
-        <DivContent bg="weballin-logo_default_m.png" />
+        <DivContent bg="weballin-logo_default_m.png" slide1 />
       </div>
       <div className="col-span-3 row-span-3">
         <DivContent bg="gangnam_hover_L@2x.webp" filter />
       </div>
       <div className="col-span-2 row-span-3">
-        <DivContent bg="client_default_L.webp" />
+        <DivContent bg="client_default_L.webp" slide2 />
       </div>
       <div className="col-span-1 row-span-1">
-        <DivContent bg="20180628_default_L.webp" />
+        <DivContent
+          bg="20180628_default_L.webp"
+          hoverBg="20180628_hover_L.webp"
+        />
       </div>
       <div className="col-span-1 row-span-2 bg-white rounded-[1vw]">
         <DivContent bg="t1.png" hoverBg="t2.png" contain />
       </div>
       <div className="col-span-5 row-span-1">
-        <DivContent bg="email_default_L.webp" />
+        <DivContent bg="email_default_L.webp" hoverBg="email_hover_L@2x.png" />
       </div>
       <div className="col-span-1 row-span-1">
-        <DivContent bg="Scratch_default_L.webp" />
+        <DivContent
+          bg="Scratch_default_L.webp"
+          hoverBg="Scratch_hover_L.webp"
+        />
       </div>
       <div className="col-span-1 row-span-1">
-        <DivContent bg="Lucky_default_L.webp" />
+        <DivContent bg="Lucky_default_L.webp" hoverBg="Lucky_hover_L.webp" />
       </div>
       <div className="col-span-5 row-span-1">
-        <DivContent bg="phone_default_L.webp" />
+        <DivContent bg="phone_default_L.webp" hoverBg="phone_hover_L@2x.png" />
       </div>
       <div className="col-span-1 row-span-1">
-        <DivContent bg="ceo_default_L.webp" />
+        <DivContent bg="ceo_default_L.webp" hoverBg="ceo-hover.png" />
       </div>
-      <div className="col-span-2 row-span-1">
+      <div className="col-span-2 row-span-1 relative group">
         <DivContent bg="partner_default_L.webp" />
+        <video
+          className="w-full h-full absolute inset-0 group-hover:block hidden rounded-[1vw] object-fill"
+          src="/div/partner_hover_L.webm"
+          autoPlay
+          loop
+          muted
+        />
       </div>
       <div className="col-span-3 row-span-1">
         <DivContent bg="copyright_default_L.png" />
