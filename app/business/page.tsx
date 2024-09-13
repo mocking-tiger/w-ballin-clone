e26f66c12dbf6bf3a6a09d23b1d2bc7545abe01d
Park JunYong <dyden119@gmail.com>
2024-09-13 21:43:54 +0900

    design: 비즈니스 페이지 첫번째 article

--- app/business/page.tsx
+++ app/business/page.tsx
@@ -1,3 +1,16 @@
 export default function Business() {
-  return <section className="mt-[54px] md:mt-[66px]">비즈니스 페이지</section>;
+  return (
+    <section className="mt-[54px] md:mt-[66px]">
+      <article className="w-full h-[240px] md:h-[400px] lg:h-[600px] bg-business1 bg-cover bg-[center]">
+        <div className="w-full h-full flex flex-col justify-center bg-businessShadow">
+          <h2 className="w-[90%] lg:w-[70%] mx-auto relative top-0 lg:-top-[30px] text-[2.4rem] md:text-[3.2rem] lg:text-[5.6rem] text-white animate-slideIn">
+            Web all in
+          </h2>
+          <p className="w-[90%] lg:w-[70%] mx-auto text-[1.4rem] md:text-[1.6rem] lg:text-[2rem] text-white opacity-0 animate-fadeIn">
+            We ballin on a Design, Marketing, and Development
+          </p>
+        </div>
+      </article>
+    </section>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,12 +15,40 @@ const config: Config = {
       backgroundImage: {
         toggle: "url(/menu-toggle.png)",
         toggleBusiness: "url(/menu-toggle@2x.png)",
+        business1: "url(/business/banner-l.webp)",
+        businessShadow:
+          "linear-gradient(95deg, #080808 -19%, rgba(8, 8, 8, 0) 93%);",
       },
       backgroundSize: {
         "100%": "100%",
       },
       backgroundPosition: {
-        right: "-11rem",
+        fix: "0px",
+        businessRight: "-450px",
+      },
+      animation: {
+        slideIn: "slideIn 0.7s ease",
+        fadeIn: "fadeIn 0.7s linear 0.7s forwards",
+      },
+      keyframes: {
+        slideIn: {
+          from: {
+            transform: "translateY(130%)",
+            opacity: "0%",
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: "100%",
+          },
+        },
+        fadeIn: {
+          from: {
+            opacity: "0%",
+          },
+          to: {
+            opacity: "100%",
+          },
+        },
       },
     },
   },
