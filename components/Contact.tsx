ac69a63a6e48179d6f6693c71532b59500936943
Park JunYong <dyden119@gmail.com>
2024-09-19 18:54:08 +0900

    chore: 모달 파일분리

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import CheckBox from "./CheckBox";
 import ContactInput from "./ContactInput";
+import ModalForContact from "./ModalForContact";
 
 export default function Contact() {
   const [budget, setBudget] = useState(100);
@@ -108,64 +109,7 @@ export default function Contact() {
           </span>
         </div>
       </form>
-      <div
-        className={`w-[400px] lg:w-auto max-w-[800px] h-auto max-h-[732px] p-[16px] lg:p-[40px] fixed z-20 left-16 bottom-16 border-2 bg-black text-white rounded-[1vw] ${
-          thisModal ? "block" : "hidden"
-        }`}
-      >
-        <div
-          className={`w-full lg:w-[712px] h-auto max-h-[574px] border-2 rounded-[1vw] overflow-hidden relative bg-white text-black`}
-        >
-          <div className="p-5">
-            <div className="mb-10">
-              <p className="text-[1.8rem] mb-5">개인정보 취급방침</p>
-              <p className="text-[1.5rem] mb-10">
-                (주)W볼린(이하’회사’라 함) 고객님의 개인정보를 중요시하며,
-                ‘정보통신망 이용 촉진 및 정보보호’에관한 법률 을 준수합니다.
-                회사는 고객문의 접수를 통해 작성해주신 고객님의 개인정보에
-                대한수집, 이용, 보유기간을 아래와 같이 안내 하오며, 문의사항에
-                대한 답변을 드리기 위해서는 개인정보 수집 및 이용에 고객님의
-                동의가 필요하오니 아래 내용을 참고하여주시기 바랍니다.
-              </p>
-              <div className="flex flex-wrap justify-between text-[1.5rem]">
-                <div>
-                  <p className="mb-3">목적</p>
-                  <p className="mb-3">본인여부 확인</p>
-                  <p className="mb-3">문의에 대한 답변</p>
-                </div>
-                <div>
-                  <p className="mb-3">항목</p>
-                  <p className="mb-3">성명</p>
-                  <p className="mb-3">
-                    업체명, 연락처, 이메일, 제목, 현 사이트/맵 주소, 요청사항
-                  </p>
-                </div>
-                <div>
-                  <p className="mb-3">보유기간</p>
-                  <p className="mb-3">답변완료 후 5일까지</p>
-                  <p className="mb-3">답변완료 후 5일까지</p>
-                </div>
-              </div>
-            </div>
-            <hr className="border-black mb-10" />
-            <div className="pop-bottom">
-              <p className="text-[1.4rem] text-[#6f6f6f]">
-                *구색 갖추기용으로 작성한 글이며 위 내용은 다 뻥입니다.
-              </p>
-            </div>
-          </div>
-        </div>
-        <p className="text-[2rem] lg:text-[2.4rem] tracking-tighter">{""}</p>
-        <div
-          className="px-[18px] py-[8px] absolute left-0 -top-28 bg-black border-2 rounded-[1vw] cursor-pointer underline text-[2rem]"
-          onClick={(e) => {
-            e.stopPropagation();
-            setThisModal((prev) => !prev);
-          }}
-        >
-          Click here to Close
-        </div>
-      </div>
+      <ModalForContact thisModal={thisModal} setThisModal={setThisModal} />
     </div>
   );
 }
